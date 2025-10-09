ab1e70c9c576861bdf422ecd4c614361eb52e5b3
tgkinzel-sudo <tgkinzel@gmail.com>
2025-10-09 11:42:09 +0200

    Create page.tsx

---
+++ app/nextjs/app/checkout/cancel/page.tsx
@@ -0,0 +1,8 @@
+export default function CancelPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Abgebrochen</h1>
+      <p>Du hast den Checkout abgebrochen.</p>
+    </main>
+  );
+}
